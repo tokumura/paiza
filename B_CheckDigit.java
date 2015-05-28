c79e86acddf9a95ba93621fff41fb0d91e024ffd
Tsuyoshi Okumura <t-okumura@san-ei-med.co.jp>
2015-05-28 18:06:57 +0900

    fix

--- B_CheckDigit.java
+++ B_CheckDigit.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class B_CheckDigit {
-	
-	public static void main(String args[]) throws Exception {
+    
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num_of_cards = Integer.parseInt(br.readLine());
         
@@ -17,55 +17,54 @@ public class B_CheckDigit {
         }
         
         for(String cn : card_list) {
-            String disit = ShowCheckDisit(new StringBuilder(cn).reverse().toString());
-            System.out.println(disit);
+            String digit = ShowCheckDigit(new StringBuilder(cn).reverse().toString());
+            System.out.println(digit);
         }
-	}
-	
-	/*
-	 * チェックディジッド表示
-	 */
-	public static String ShowCheckDisit(String card_num) throws Exception {
-		// 偶数
+    }
+    
+    /*
+     * チェックディジッド表示
+     */
+    public static String ShowCheckDigit(String card_num) throws Exception {
+        // 偶数
         int idx = 1;
         int even_total = 0;
         for(int i = 1; i <= 8; i++) {
-        	String number = card_num.substring(idx, idx + 1);
-        	int number_doubled = Integer.parseInt(number) * 2;
-        	if (number_doubled >= 10) {
-        		int ones = Integer.parseInt(String.valueOf(number_doubled).substring(0, 1));
-        		int tens = Integer.parseInt(String.valueOf(number_doubled).substring(1, 2));
-        		even_total = even_total + ones + tens;
-        	} else {
-        		even_total += number_doubled;
-        	}
-        	idx += 2;
+            String number = card_num.substring(idx, idx + 1);
+            int number_doubled = Integer.parseInt(number) * 2;
+            if (number_doubled >= 10) {
+                int ones = Integer.parseInt(String.valueOf(number_doubled).substring(0, 1));
+                int tens = Integer.parseInt(String.valueOf(number_doubled).substring(1, 2));
+                even_total = even_total + ones + tens;
+            } else {
+                even_total += number_doubled;
+            }
+            idx += 2;
         }
         
         // 奇数
         idx = 0;
         int odd_total = 0;
         for(int i = 0; i < 8; i++) {
-        	String number = card_num.substring(idx, idx + 1);
+            String number = card_num.substring(idx, idx + 1);
 
-        	if (number.equals("X")) {
-        		idx += 2;
-        		continue;
-        	}
-        	
-        	odd_total += Integer.parseInt(number);
-        	idx += 2;
+            if (number.equals("X")) {
+                idx += 2;
+                continue;
+            }
+            
+            odd_total += Integer.parseInt(number);
+            idx += 2;
         }
         
         // Xの値
         int total = even_total + odd_total;
-        int disit = 0;
+        int digit = 0;
         int rest = total % 10;
         if (rest != 0) {
-        	disit = 10 - rest;
+            digit = 10 - rest;
         }
         
-        return String.valueOf(disit);
-	}
-
+        return String.valueOf(digit);
+    }
 }
